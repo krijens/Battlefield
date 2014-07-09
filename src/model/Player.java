1b17ff78e4659e8646d1a327ce22db98a7715c92
krijens <kristofferje@gmail.com>
2014-07-09 22:56:29 +0200

    Init

---
+++ src/model/Player.java
@@ -0,0 +1,11 @@
+package model;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: kje
+ * Date: 09.07.14
+ * Time: 22:56
+ * To change this template use File | Settings | File Templates.
+ */
+public class Player {
+}
